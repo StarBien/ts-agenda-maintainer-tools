916c2f94817b0e99ca5ebf893a21f891df33c347
Guido Castrogiovanni <nicocastrogiovanni@hotmail.com>
2024-12-26 16:18:18 -0300

    Implement builder pattern on Branch & Medic

--- src/utils/CLINI/generate-sql-files.ts
+++ src/utils/CLINI/generate-sql-files.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-import { Branch, Clinic } from "../../types/schemas/medical";
+import { Branch, BranchBuilder, Clinic } from "../../types/schemas/medical";
 
 
 export default function execute() {
@@ -8,6 +8,14 @@ export default function execute() {
     let clinic = new Clinic('Test Clinic');
 
     console.log(genClinic(clinic))
+
+    let [branch, branchSQL] = genBranch(new BranchBuilder()
+        .setClinicId(clinic.id)
+        .setName('Test Branch')
+        .build()
+    )
+
+    console.log(branch, branchSQL)
 
 } 
 
@@ -32,8 +40,36 @@ export const genClinic = (clinic : Clinic) => {
 
 }
 
-export const genBranch = (branch : Branch) => {
+export const genBranch = (branch : Branch) : Object[] => {
 
-    
+    let template = `
+        -- ADD BRANCH ${branch.getName().toUpperCase()}
+        INSERT INTO medical.branches
+        (id, clinic_id, name, country, region, commune, city, street_name, street_number, rest_of_address, latitude, longitude, altitude, phone)
+        VALUES
+        (
+            '${branch.getId()}',
+            '${branch.getClinicId()}',
+            '${branch.getName()}',
+            'CL',
+            'CL-RM',
+            'CL PRO',
+            'Santiago',
+            'Luis Thayer Ojeda',
+            ${branch.getStreetNumber()},
+            '${branch.getRestOfAddress()}',
+            -33.4179992,
+            -70.6032104,
+            0,
+            '+56229944003'
+        ) ON CONFLICT DO NOTHING;
+        `;
 
+    return [branch, template];
+
+}
+
+type wrapperSQL = {
+    object : any
+    sql : string
 }
