c7c412b3d39919c6e42e87d37de1d6f68fbfa556
Guido Castrogiovanni <nicocastrogiovanni@hotmail.com>
2025-01-15 12:57:32 -0300

    Implement Mapping Logic

--- src/services/Process.ts
+++ src/services/Process.ts
@@ -4,6 +4,8 @@ import { inputData, inputMap, InputType } from "./FetchInput"
 
 import { BranchDTO, SpecialtyDTO, ClinicDTO, PractitionerPersonDTO, ClinicResourceDTO,  } from "./types"
 
+import { consolidateInsertStatements } from "../utils/consolidate-sql-statements"
+
 
 // TODO Implementar ZOD o algun Schema Validator
 
@@ -48,9 +50,83 @@ function create() {
         sql += genSQL.createBranch(sede)
     })
     
-    inputMap.Sedes
+    // console.log('inputData.Sedes: ', inputMap.Sedes)
+
+    // Run Sedes Map
+    Object.entries(inputMap.Sedes).forEach(kvp => {
+        const sedeScriptId = kvp[0]
+        const {Practicantes, Recursos} = kvp[1]
+
+        const sede = inputData.Sedes.find(x => x.SCRIPT_ID.toString() == sedeScriptId)
+        if(sede == undefined) return
+        
+        // Link Practitioners to Branches
+        Practicantes.toString().split(';').forEach(scriptId => {
+
+            if(scriptId == '') return   // Empty cell
+
+            const practicante = inputData.Practicantes.find(x => x.SCRIPT_ID.toString() == scriptId)
+            if(practicante == undefined) throw new Error(`[Sedes Map] - No se encontró el practicante con Script ID '${scriptId}'`)
+
+            sql += genSQL.addPractitionerToBranch(sede.id, practicante.id)
+
+        })
+        
+        // Link Resources to Branches
+        Recursos.toString().split(';').forEach(scriptId => {
+
+            if(scriptId == '') return   // Empty cell
+            
+            const recurso = inputData.Recursos.find(x => x.SCRIPT_ID.toString() == scriptId)
+            if(recurso == undefined) throw new Error(`[Sedes Map] - No se encontró el recurso con Script ID '${scriptId}'`)
+
+            sql += genSQL.addResourceToBranch(sede.id, recurso.id)
+
+        })
+    })
+
+
+    // Run Especialidades Map
+    Object.entries(inputMap.Especialidades).forEach(kvp => {
+        const especialidadScriptId = kvp[0]
+        const {Sedes, Recursos, Practicantes} = kvp[1]
+
+        const especialidad = inputData.Especialidades.find(x => x.SCRIPT_ID.toString() == especialidadScriptId)
+        if(especialidad == undefined) return
+
+        Sedes.toString().split(';').forEach(scriptId => {
+            if(scriptId == '') return   // Empty cell
+            
+            const sede = inputData.Sedes.find(x => x.SCRIPT_ID.toString() === scriptId)
+            if(sede == undefined) throw new Error(`[Especialidades Map] - No se encontró el sede con Script ID '${scriptId}'`)
+            
+            sql += genSQL.addSpecialtyToBranch(especialidad.snomedCode, sede.id)
+
+        })
+
+        Recursos.toString().split(';').forEach(scriptId => {
+            if(scriptId == '') return   // Empty cell
+            
+            const sede = inputData.Sedes.find(x => x.SCRIPT_ID.toString() === scriptId)
+            if(sede == undefined) throw new Error(`[Especialidades Map] - No se encontró la sede con Script ID '${scriptId}'`)
+            
+            sql += genSQL.addSpecialtyToBranch(especialidad.snomedCode, sede.id)
+
+        })
+
+        Practicantes.toString().split(';').forEach(scriptId => {
+            if(scriptId == '') return   // Empty cell
+            
+            const sede = inputData.Sedes.find(x => x.SCRIPT_ID.toString() === scriptId)
+            if(sede == undefined) throw new Error(`[Especialidades Map] - No se encontró el practicante con Script ID '${scriptId}'`)
+            
+            sql += genSQL.addSpecialtyToBranch(especialidad.snomedCode, sede.id)
+
+        })
+    })
+
     
-    console.log(sql)
+    console.log(consolidateInsertStatements([sql]).join('\n\n'))
 
 }
 
