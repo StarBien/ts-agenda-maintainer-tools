6b4dc8ae9c7f6d6ee81b747acec9ff25ff01c5c1
Guido Castrogiovanni <nicocastrogiovanni@hotmail.com>
2025-01-20 17:37:35 -0300

    Update example input

--- output/example.sql
+++ output/example.sql
@@ -1,39 +1,61 @@
+INSERT INTO medical.specialties
+(snomed_code, snomed_label, starbien_label, specialty_type, practice_type)
+VALUES
+    ('111', 'SNOME #1', 'Especialidad 1', 'CONSULTATION', 'CONSULTATION'),
+    ('222', 'SNOMED #2', 'Especialidad 2', 'LABORATORY', 'EXAM'),
+    ('333', 'SNOMED #3', 'Especialidad 3', 'IMAGING', 'EXAM')
+ ON CONFLICT DO NOTHING;
+
 INSERT INTO medical.clinics
 (id, name, has_online_booking, location_type)
 VALUES
-    ('bebfae39-eb4c-4ca5-9d96-2279ef2c55f4', 'CLINICA TEST 1', false, 'AT_FACILITIES'),
-    ('bb91397c-da3b-4cf8-9e3a-38d09da30987', 'CLINICA TEST 2', false, 'AT_FACILITIES'),
-    ('60317aaa-cad8-4c17-b5b7-c9d0f7e079ca', 'CLINICA TEST 3', false, 'AT_FACILITIES')
+    ('9a9447c4-ae99-432d-8379-7d480075ab73', 'CLINICA TEST 1', false, 'AT_FACILITIES'),
+    ('a6966952-2906-4904-a0f2-40c725184d4b', 'CLINICA TEST 2', false, 'AT_FACILITIES'),
+    ('8e5b8a11-8737-41ee-a924-7701eeccffc0', 'CLINICA TEST 3', false, 'AT_FACILITIES')
  ON CONFLICT DO NOTHING;
 
 INSERT INTO medical.branches
 (id, clinic_id, name, country, region, commune, city, street_name, street_number, rest_of_address, latitude, longitude, altitude, phone)
 VALUES
-    ('9c92392f-f14d-4dd4-8b76-4aad09eb91c8', 'C111', 'TM | Metro Los Leones', 'CL', 'CL-RM', 'CL PRO', 'Santiago', 'Coyancura', 2244, 'Esq Lyon/Lota. Edificio Paris', -706088728, -33423565, 0, '56229944003'),
-    ('a006ba8d-130d-449b-a080-a35ba51fef68', 'C111', 'TM | San Bernardo', 'CL', 'CL-RM', 'CL SBD', 'Santiago', 'OHiggins', 337, 'NULL', -707014946, -335924114, 0, '56229944003'),
-    ('03e26125-0e94-4895-ba46-1b098b091265', 'C111', 'Centro de Salud Tobalaba', 'CL', 'CL-RM', 'CL PRO', 'Santiago', 'Luis Thayer Ojeda', 73, 'Piso 2. A pasos del Costanera Center', -706032104, -334179992, 0, '56229944003'),
-    ('f3a1ca3e-b40a-49c9-a8f6-10bef7cbea32', 'C222', 'Nueva Sede', 'CL', 'CL-RM', 'CL PRO', 'Santiago', 'Calle Magica', 777, 'NULL', -706032104, -334179992, 0, 'NULL')
+    ('4bae4d51-460a-4b1a-a314-c3f0cec5d7de', '9a9447c4-ae99-432d-8379-7d480075ab73', 'TM | Metro Los Leones', 'CL', 'CL-RM', 'CL PRO', 'Santiago', 'Coyancura', 2244, 'Esq Lyon/Lota. Edificio Paris', -70.6088728, -33.423565, 0, '56229944003'),
+    ('afaf6497-ec20-4eb2-a672-c16acd396206', '9a9447c4-ae99-432d-8379-7d480075ab73', 'TM | San Bernardo', 'CL', 'CL-RM', 'CL SBD', 'Santiago', 'O''Higgins', 337, 'NULL', -70.7014946, -33.5924114, 0, '56229944003'),
+    ('29b4e69b-eeb3-45ce-abf3-13a30e322208', 'a6966952-2906-4904-a0f2-40c725184d4b', 'Centro de Salud Tobalaba', 'CL', 'CL-RM', 'CL PRO', 'Santiago', 'Luis Thayer Ojeda', 73, 'Piso 2. A pasos del Costanera Center', -70.6032104, -33.4179992, 0, '56229944003'),
+    ('6d217f6a-7081-41c9-93cc-c576040f5a69', 'dcf6a6e2-3559-436f-96c5-c45c126f777', 'Nueva Sede', 'CL', 'CL-RM', 'CL PRO', 'Santiago', 'Calle Magica', 777, 'NULL', -70.6032104, -33.4179992, 0, 'NULL')
+ ON CONFLICT DO NOTHING;
+
+INSERT INTO persons.persons
+(id, firstname, lastname, mother_maiden_name, birthdate, country, region, commune, city, street_name, street_number, rest_of_address, document_type, document_value, document_country, gender, deceased, phone, email)
+VALUES
+    ('a01aa77f-a776-4cb8-b89e-9fbf4d821933', 'Juan', 'Perez', 'Rodriguez', NULL, 'CL', NULL, NULL, '', '', NULL, NULL, 'RUT', 'NULL', 'CL', NULL, 'false', '', 'NULL'),
+    ('cea5dc1a-0db4-4f50-8f0c-6cdfbfd8a307', 'Terminator', 'T-1000', 'O''Connor', NULL, 'CL', NULL, NULL, '', '', NULL, NULL, 'RUT', 'NULL', 'CL', NULL, 'false', '', 'NULL')
+ ON CONFLICT DO NOTHING;
+
+INSERT INTO medical.medics
+(id, photo, attendance_type, location_type)
+VALUES
+    ('a01aa77f-a776-4cb8-b89e-9fbf4d821933', 'https://www.starbien.life/images/generic_practitioner_icon.png.png', 'IN_PERSON', 'AT_FACILITIES'),
+    ('cea5dc1a-0db4-4f50-8f0c-6cdfbfd8a307', 'https://www.starbien.life/images/generic_practitioner_icon.png.png', 'IN_PERSON', 'AT_FACILITIES')
  ON CONFLICT DO NOTHING;
 
 INSERT INTO medical.medics_branches
 (branch_id, medic_id)
 VALUES
-    ('9c92392f-f14d-4dd4-8b76-4aad09eb91c8', '82604d85-23ef-4631-8fb3-fd483039d054'),
-    ('a006ba8d-130d-449b-a080-a35ba51fef68', '82604d85-23ef-4631-8fb3-fd483039d054'),
-    ('f3a1ca3e-b40a-49c9-a8f6-10bef7cbea32', '50954d1f-2351-4d7a-9671-e4dcc96c0a3e')
+    ('4bae4d51-460a-4b1a-a314-c3f0cec5d7de', 'cea5dc1a-0db4-4f50-8f0c-6cdfbfd8a307'),
+    ('afaf6497-ec20-4eb2-a672-c16acd396206', 'cea5dc1a-0db4-4f50-8f0c-6cdfbfd8a307'),
+    ('6d217f6a-7081-41c9-93cc-c576040f5a69', 'a01aa77f-a776-4cb8-b89e-9fbf4d821933')
  ON CONFLICT DO NOTHING;
 
 INSERT INTO medical.branches_specialties
 (branch_id, specialty_id)
 VALUES
-    ('9c92392f-f14d-4dd4-8b76-4aad09eb91c8', '111'),
-    ('a006ba8d-130d-449b-a080-a35ba51fef68', '111'),
-    ('03e26125-0e94-4895-ba46-1b098b091265', '111'),
-    ('a006ba8d-130d-449b-a080-a35ba51fef68', '111'),
-    ('9c92392f-f14d-4dd4-8b76-4aad09eb91c8', '222'),
-    ('a006ba8d-130d-449b-a080-a35ba51fef68', '222'),
-    ('03e26125-0e94-4895-ba46-1b098b091265', '222'),
-    ('a006ba8d-130d-449b-a080-a35ba51fef68', '222'),
-    ('03e26125-0e94-4895-ba46-1b098b091265', '333'),
-    ('f3a1ca3e-b40a-49c9-a8f6-10bef7cbea32', '333')
+    ('4bae4d51-460a-4b1a-a314-c3f0cec5d7de', '111'),
+    ('afaf6497-ec20-4eb2-a672-c16acd396206', '111'),
+    ('29b4e69b-eeb3-45ce-abf3-13a30e322208', '111'),
+    ('afaf6497-ec20-4eb2-a672-c16acd396206', '111'),
+    ('4bae4d51-460a-4b1a-a314-c3f0cec5d7de', '222'),
+    ('afaf6497-ec20-4eb2-a672-c16acd396206', '222'),
+    ('29b4e69b-eeb3-45ce-abf3-13a30e322208', '222'),
+    ('afaf6497-ec20-4eb2-a672-c16acd396206', '222'),
+    ('29b4e69b-eeb3-45ce-abf3-13a30e322208', '333'),
+    ('6d217f6a-7081-41c9-93cc-c576040f5a69', '333')
  ON CONFLICT DO NOTHING;
--- src/services/InputMapper.ts
+++ src/services/InputMapper.ts
@@ -1,120 +1,99 @@
-import { z } from "zod"
-import { BranchDTO, SpecialtyDTO, ClinicDTO, PractitionerPersonDTO, ClinicResourceDTO,  } from "./types"
+import { z } from "zod";
 import { sqlEscapeSingleQuotes } from "@src/utils/sql-escape-single-quotes";
 
 // Type for normalization configuration
 type NormalizationConfig = {
-	[key: string]: "string" | "number" | "date" | "unknown";
-  };
-  
-  // Map Zod schema types to normalizationConfig types
- function generateNormalizationConfig(schema: z.ZodSchema): NormalizationConfig {
-	const shape = (schema as z.ZodObject<any>).shape;
-	const config: NormalizationConfig = {};
-  
-	for (const key in shape) {
-		const field = shape[key];
-		if (field instanceof z.ZodString) {
-			config[key] = "string";
-		} else if (field instanceof z.ZodNumber) {
-			config[key] = "number";
-		} else if (field instanceof z.ZodDate) {
-			config[key] = "date";
-		} else {
-			config[key] = "unknown"; // Default for unsupported types
-		}
-	}
-  
-	return config;
+   [key: string]: "string" | "number" | "date" | "unknown";
+};
+
+// Map Zod schema types to normalizationConfig types
+function generateNormalizationConfig(schema: z.ZodSchema): NormalizationConfig {
+   const shape = (schema as z.ZodObject<any>).shape;
+   const config: NormalizationConfig = {};
+
+   for (const key in shape) {
+      const field = shape[key];
+      if (field instanceof z.ZodString) {
+         config[key] = "string";
+      } else if (field instanceof z.ZodNumber) {
+         config[key] = "number";
+      } else if (field instanceof z.ZodDate) {
+         config[key] = "date";
+      } else {
+         config[key] = "unknown"; // Default for unsupported types
+      }
+   }
+
+   return config;
 }
 
 // Function to normalize data based on the configuration
 export function normalizeData(
-  data: Record<string, any>[],
-  schema: z.ZodObject<any>
+   data: Record<string, any>[],
+   schema: z.ZodObject<any>
 ): [Record<string, any>[], boolean] {
+   const config = generateNormalizationConfig(schema);
 
-	const config = generateNormalizationConfig(schema)
+   const keyMap = createKeyMap(data[0]);
 
-	const keyMap = createKeyMap(data[0]);
+   const translated = data.map((row) => {
+      const normalizedRow: Record<string, any> = {};
 
-	const translated = data.map((row) => {
-		const normalizedRow: Record<string, any> = {};
+      row = mapKeys(row, keyMap);
 
-		row = mapKeys(row, keyMap)
+      for (const key in row) {
+         if (config[key]) {
+            switch (config[key]) {
+               case "string":
+                  normalizedRow[key] = sqlEscapeSingleQuotes(row[key]?.toString()) ?? "";
+                  break;
+               case "number":
+                  normalizedRow[key] = !isNaN(Number(row[key])) ? Number(row[key]) : null;
+                  break;
+               case "date":
+                  normalizedRow[key] = new Date(row[key]);
+                  if (isNaN(normalizedRow[key].getTime())) {
+                     normalizedRow[key] = null; // Invalid dates become null
+                  }
+                  break;
+               default:
+                  normalizedRow[key] = row[key]; // Preserve original value for unknown types
+            }
+         } else {
+            normalizedRow[key] = row[key]; // Preserve values for fields not in config
+         }
+      }
+      return normalizedRow;
+   });
 
-		for (const key in row) {
-			if (config[key]) {
-				switch (config[key]) {
-					case "string":
-						normalizedRow[key] = sqlEscapeSingleQuotes(row[key]?.toString()) ?? "";
-						break;
-					case "number":
-						normalizedRow[key] = !isNaN(Number(row[key])) ? Number(row[key]) : null;
-						break;
-					case "date":
-						normalizedRow[key] = new Date(row[key]);
-						if (isNaN(normalizedRow[key].getTime())) {
-						normalizedRow[key] = null; // Invalid dates become null
-						}
-						break;
-					default:
-						normalizedRow[key] = row[key]; // Preserve original value for unknown types
-				}
-			} else {
-				normalizedRow[key] = row[key]; // Preserve values for fields not in config
-			}
-		}
-		return normalizedRow;
-  });
+   const success = translated.every((row) => schema.safeParse(row));
 
-	const success = translated.every(row => schema.safeParse(row))
-
-	return [translated, success]
+   return [translated, success];
 }
-
 
 // =====================================
 
-export function mapKeys<T extends Record<string, any>> (
-	data: T,
-	keyMap: Record<string, string>
-) : Record<string, any> {
-	return Object.fromEntries(
-		Object.entries(data).map(([key, value]) => [keyMap[key] || key, value])
-	  );
+export function mapKeys<T extends Record<string, any>>(
+   data: T,
+   keyMap: Record<string, string>
+): Record<string, any> {
+   return Object.fromEntries(
+      Object.entries(data).map(([key, value]) => [keyMap[key] || key, value])
+   );
 }
 
 // Utility function to convert snake_case to camelCase
 function snakeToCamel(snake: string): string {
-	return snake.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
-  }
-  
-  // Function to create the keyMap
-  export function createKeyMap<T extends Record<string, any>>(data: T): Record<string, string> {
-		const keyMap: Record<string, string> = {};
-		for (const key of Object.keys(data)) {
-			if (/_/.test(key)) {
-			keyMap[key] = snakeToCamel(key);
-			}
-		}
-		return keyMap;
-  }
+   return snake.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+}
 
-export const BranchKeyMap = {
-	SCRIPT_ID : 'SCRIPT_ID',
-	id : 'id',
-	clinic_id : 'clinicId',
-	name : 'name',
-	country : 'country',
-	region : 'region',
-	commune : 'commune',
-	city : 'city',
-	street_name : 'streetName',
-	street_number : 'streetNumber',
-	rest_of_address : 'restOfAddress',
-	latitude : 'latitude',
-	longitude : 'longitude',
-	altitude : 'altitude',
-	phone : 'phone',
+// Function to create the keyMap
+export function createKeyMap<T extends Record<string, any>>(data: T): Record<string, string> {
+   const keyMap: Record<string, string> = {};
+   for (const key of Object.keys(data)) {
+      if (/_/.test(key)) {
+         keyMap[key] = snakeToCamel(key);
+      }
+   }
+   return keyMap;
 }
